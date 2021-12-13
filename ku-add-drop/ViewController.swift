e4691d3ef609f7393bb950b4c755fad5a2b0bc7d
bozercavdar <83980769+bozercavdar@users.noreply.github.com>
2021-12-13 21:33:03 +0300

    initial commit

---
+++ ku-add-drop/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ku-add-drop
+//
+//  Created by Lab on 13.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var userNameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func login(_ sender: UIButton) {
+    }
+    
+}
+
