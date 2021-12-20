c6f56e5927609ce5169b40deb607956e00cfcffe
Lab <bcavdar17@ku.edu.tr>
2021-12-20 12:59:31 +0300

    Add SignupViewController

--- ku-add-drop/View Controllers/SignUpPageViewController.swift
+++ ku-add-drop/View Controllers/SignUpPageViewController.swift
@@ -1,34 +1,24 @@
 //
-//  LoginPageViewController.swift
+//  SignUpPageViewController.swift
 //  ku-add-drop
 //
-//  Created by Lab on 19.12.2021.
+//  Created by Lab on 20.12.2021.
 //
 
 import UIKit
 import Firebase
-import FirebaseAuth
 
-class LoginPageViewController: UIViewController {
+class SignUpPageViewController: UIViewController {
 
-    @IBOutlet weak var usernameLabel: UITextField!
-    @IBOutlet weak var passwordLabel: UITextField!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    @IBAction func loginAction(_ sender: Any) {
-        let username = usernameLabel.text!
-        let password = passwordLabel.text!
-        print(username, password)
-        
-        FirebaseAuth.Auth.auth().signIn(withEmail: username, password: password, completion: {result, error in
+    func signup(){
+        FirebaseAuth.Auth.auth().createUser(withEmail: "username", password: "password", completion: {result, error in
             guard error == nil else {
-                print("wrong credentials")
                 return
             }
             let storyboard = UIStoryboard(name: "Main", bundle: nil)
@@ -38,9 +28,9 @@ class LoginPageViewController: UIViewController {
             // then call the change root view controller function to change to main tab bar
             (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(mainTabBarController)
         })
-        
     }
     
+
     /*
     // MARK: - Navigation
 
