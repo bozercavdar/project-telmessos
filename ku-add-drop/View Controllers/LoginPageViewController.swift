1bda361dc930c8aba0803dca97f4c0e90deee853
tguler17 <tguler17@ku.edu.tr>
2022-01-18 22:23:11 +0300

    Tried to implement staying logged in
    Co-Authored-By: Murat Han <83009554+maydogan17@users.noreply.github.com>
    Co-Authored-By: bozercavdar <83980769+bozercavdar@users.noreply.github.com>

--- ku-add-drop/View Controllers/LoginPageViewController.swift
+++ ku-add-drop/View Controllers/LoginPageViewController.swift
@@ -11,14 +11,39 @@ import FirebaseAuth
 
 class LoginPageViewController: UIViewController {
 
+//    let userDefault = UserDefaults.standard
+//    let launchedBefore = UserDefaults.standard.bool(forKey: "usersignedin")
+    
     @IBOutlet weak var usernameLabel: UITextField!
     @IBOutlet weak var passwordLabel: UITextField!
     var iconClick = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
+    
+//    override func viewWillAppear(_ animated: Bool) {
+//        let ifLoggedIn = userDefault.bool(forKey: "loggedIn")
+//        let savedUsername = userDefault.string(forKey: "username")
+//        let savedPassword = userDefault.string(forKey: "password")
+//        if ifLoggedIn {
+//            let credential = EmailAuthProvider.credential(withEmail: savedUsername!, password: savedPassword!)
+//            Auth.auth().signInAndRetrieveData(with: credential, completion: {result, error in
+//                if error == nil {
+//                    self.userDefault.set(true, forKey: "loggedin")
+//                    self.userDefault.synchronize()
+//                    print(result?.user.email)
+//                }
+//            })
+//            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//            let mainTabBarController = storyboard.instantiateViewController(identifier: "MainTabBarController")
+//
+//            // This is to get the SceneDelegate object from your view controller
+//            // then call the change root view controller function to change to main tab bar
+//            (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(mainTabBarController)
+//        }
+//    }
     
     @IBAction func shortcut(_ sender: Any) {
         FirebaseAuth.Auth.auth().signIn(withEmail: "fbulgur17@ku.edu.tr", password: "123456", completion: {result, error in
@@ -47,13 +72,17 @@ class LoginPageViewController: UIViewController {
     @IBAction func loginAction(_ sender: Any) {
         let username = usernameLabel.text!
         let password = passwordLabel.text!
-        print(username, password)
+//        credential = EmailAuthProvider.credential(withEmail: username, password: password)
         
         FirebaseAuth.Auth.auth().signIn(withEmail: username, password: password, completion: {result, error in
             guard error == nil else {
                 print("wrong credentials")
                 return
             }
+//            self.userDefault.set(true, forKey: "loggedIn")
+//            self.userDefault.set(username, forKey: "username")
+//            self.userDefault.set(password, forKey: "password")
+            
             let storyboard = UIStoryboard(name: "Main", bundle: nil)
             let mainTabBarController = storyboard.instantiateViewController(identifier: "MainTabBarController")
 
@@ -62,6 +91,7 @@ class LoginPageViewController: UIViewController {
             (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(mainTabBarController)
         })
         
+
     }
     
     /*
